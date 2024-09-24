6623bb17e156526a781f73cf0c4efcd5b4df4b84
Javier Ruiz <javier.ruiz.serrano59@gmail.com>
2024-09-24 14:06:56 +0200

    Test de storage realizados en el proyecto de exceptions

--- src/test/kotlin/tenistas/validators/CsvValidatorKtTest.kt
+++ src/test/kotlin/tenistas/validators/CsvValidatorKtTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.assertThrows
-import tenistas.exceptions.ArgsException
 import tenistas.exceptions.CsvException
 
 class CsvValidatorKtTest {
